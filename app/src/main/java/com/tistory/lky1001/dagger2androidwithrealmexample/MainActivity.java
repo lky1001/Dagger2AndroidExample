99becc62a7a2751b4b7f0dbc8f72cd08f9d9493f
kylee <kylee@devband.com>
2016-08-16 09:20:31 +0900

    안드로이드 스튜디오로 프로젝트 생성 dagger2, realm 디펜던시 설정

---
+++ app/src/main/java/com/tistory/lky1001/dagger2androidwithrealmexample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tistory.lky1001.dagger2androidwithrealmexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
